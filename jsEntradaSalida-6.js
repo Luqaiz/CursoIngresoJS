6a99b17411dc31fd1cb580c97f0d35091e4e1a4c
Luqaiz <romerolucas28@gmail.com>
2017-02-02 11:42:34 -0300

    Ejercicio numero 6
    suma con PARSEINT

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{ 
+var numeroUno
+var numeroDos
+
+numeroUno = document.getElementById('numeroUno').value
+numeroDos = document.getElementById("numeroDos").value
+
+alert (parseInt (numeroUno) + parseInt (numeroDos));
+}
+
