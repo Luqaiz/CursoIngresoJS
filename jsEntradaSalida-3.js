fa760234c810887414a2e671ca84f43d1bd9a9b8
Luqaiz <romerolucas28@gmail.com>
2017-02-02 09:53:40 -0300

    Ejercicio numero 3 echo en clase
    corrección del ejercicio numero 3

--- jsEntradaSalida-3.js
+++ jsEntradaSalida-3.js
@@ -1,7 +1,9 @@
 /*Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{ 
-var p= document.getElementById("elNombre").value
-alert (p);
+{
+//corregido en clase 
+var nombre;
+nombre = document.getElementById('elNombre').value;
+alert ("el nombre ingresado es " + nombre);
 } 
