efba59c53db37f9f85dc9287905990ba8e144cef
Luqaiz <romerolucas28@gmail.com>
2017-02-02 09:16:41 -0300

    ejercicios
    ejercicios terminados 2/2

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,6 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{ 
+var m = "esto funciona de maravilla"
+alert (m);
+}
---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{
+var p = document.getElementById("elNombre").value
+alert (p)
+var b = document.getElementById("laEdad").value=66
+ 
+}
+
