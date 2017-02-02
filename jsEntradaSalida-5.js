250fe643a91c65d22d564c294065154b13170f04
Luqaiz <romerolucas28@gmail.com>
2017-02-02 11:37:43 -0300

    Ejercicio numero 5
    CONTATENAR con +

--- jsEntradaSalida-5.js
+++ jsEntradaSalida-5.js
@@ -2,9 +2,10 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {
-var p = document.getElementById("elNombre").value
-alert (p)
-var b = document.getElementById("laEdad").value=66
+var nombre;
+nombre = document.getElementById("elNombre").value;
+var edad;
+edad = document.getElementById("laEdad").value;
+alert ("Usted se llama " + nombre + " y tiene " + edad +" años")
  
 }
-
