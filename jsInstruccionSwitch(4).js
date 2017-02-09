7e2779c5ae2f89a86324f98f38786734922ec805
Luqaiz <romerolucas28@gmail.com>
2017-02-09 12:10:31 -0300

    EJERCICIOS SWITCH
    todos los excercises

---
+++ jsInstruccionSwitch(4).js
@@ -0,0 +1,27 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+	switch (mesDelAño)
+	{
+		case "Febrero":
+		alert ("Tiene 28 días")
+		break;
+
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+		alert ("Tiene 31 días")
+		break;
+
+		default:
+		alert ("Tiene 30 días")
+		break;
+	}
+}//FIN DE LA FUNCIÓN
